05be66e63f0da4c77e0e38c0507f3fa775959bc2
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2012-11-25 12:31:50 +0800

    Dictionaries initialisation

--- Portal/Авиашельф-портал/kwf-aviashelf/app/Acl.php
+++ Portal/Авиашельф-портал/kwf-aviashelf/app/Acl.php
@@ -7,8 +7,8 @@ class Acl extends Kwf_Acl
         $this->remove('default_index');
         //$this->remove('default_dictionary');
 
-        $this->addResource(new Kwf_Acl_Resource_MenuUrl('default_index', array('text'=>trl('Штат'), 'icon'=>'user.png'), '/'));
-        $this->addResource(new Kwf_Acl_Resource_MenuUrl('default_dictionary', array('text'=>trl('Справочники'), 'icon'=>'book.png'), '/'));
+        $this->addResource(new Kwf_Acl_Resource_MenuUrl('default_index', array('text'=>trl('AAA'), 'icon'=>'user.png'), '/'));
+        $this->addResource(new Kwf_Acl_Resource_MenuUrl('default_dictionary', array('text'=>trl('Dictionaries'), 'icon'=>'book.png'), '/'));
         
         $this->addResource(new Zend_Acl_Resource('default_members'), 'default_index');
         $this->addResource(new Zend_Acl_Resource('default_member'), 'default_members');
