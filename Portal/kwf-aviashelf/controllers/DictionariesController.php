f317bb27ba9136af41a27aad9950486cb5109945
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2012-11-30 09:13:41 +0800

    Dictionaries first launch

--- Portal/kwf-aviashelf/controllers/DictionariesController.php
+++ Portal/kwf-aviashelf/controllers/DictionariesController.php
@@ -8,7 +8,7 @@ class DictionariesController extends Kwf_Controller_Action_Auto_Grid
         
         protected function _initColumns()
         {
-            $this->_filters = array('text' => array('name' => 'TextField'));
+            $this->_filters = array('name' => array('type' => 'TextField'));
             $this->_columns->add(new Kwf_Grid_Column('name', trl('Title'), 200));
         }
 }
