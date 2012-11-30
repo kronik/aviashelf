b2ed8d20cc86f6bedd9969df3af68866f60e94ee
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2012-11-30 20:19:45 +0800

    JS for dictionary fixed

--- Portal/kwf-aviashelf/js/Dictionaries.js
+++ Portal/kwf-aviashelf/js/Dictionaries.js
@@ -1,36 +1,35 @@
 var Dictionaries = Ext.extend(Ext.Panel,
-                         {
-                         initComponent : function(test)
-                         {
-                          var dictionary = new Kwf.Auto.GridPanel({
-                                                                controllerUrl   : '/dictionary',
-                                                                region          : 'center',
-                                                                resizable       : true,
-                                                                split           : true,
-                                                                collapsible     : true,
-                                                                title           : trl('Dictionary')
-                                                                });
-                         
-                          var grid = new Kwf.Auto.GridPanel({
-                                                           controllerUrl   : '/dictionaries',
-                                                           region          : 'west',
-                                                           width           : 300,
-                                                           resizable       : true,
-                                                           split           : true,
-                                                           collapsible     : true,
-                                                           title           : trl('Dictionaries'),
-                                                           bindings: [dictionary, {
-                                                                      queryParam: 'name',
-                                                                      item: dictionary
-                                                                      }]
-                                                           });
-                         
-                         this.layout = 'border';
-                         this.items = [grid, {
-                                       layout: 'border',
-                                       region: 'center',
-                                       items: [dictionary]
-                                       }];
-                         Members.superclass.initComponent.call(this);
-                         }
-                         });
+{
+    initComponent : function(test)
+    {
+        var dictionary = new Kwf.Auto.GridPanel({
+                                            controllerUrl   : '/dictionary',
+                                            region          : 'center',
+                                            resizable       : true,
+                                            split           : true,
+                                            collapsible     : true,
+                                            title           : trl('Dictionary')
+                                            });
+
+        var grid = new Kwf.Auto.GridPanel({
+                                       controllerUrl   : '/dictionaries',
+                                       region          : 'west',
+                                       width           : 300,
+                                       resizable       : true,
+                                       split           : true,
+                                       collapsible     : true,
+                                       title           : trl('Dictionaries'),
+                                       bindings: [dictionary, {
+                                                  queryParam: 'name'
+                                                  }]
+                                       });
+
+        this.layout = 'border';
+        this.items = [grid, {
+                   layout: 'border',
+                   region: 'center',
+                   items: [dictionary]
+                   }];
+        Dictionaries.superclass.initComponent.call(this);
+    }
+});
