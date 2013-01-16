2396589e8d23bde6ee008ccba11ffc0c3690ca1a
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2013-01-16 10:01:40 +0800

    Fixed flight group creation

--- Portal/kwf-aviashelf/controllers/FlightgroupController.php
+++ Portal/kwf-aviashelf/controllers/FlightgroupController.php
@@ -11,7 +11,7 @@ class FlightgroupController extends Kwf_Controller_Action_Auto_Form
         $employeesSelect = $employeesModel->select()->whereEquals('visible', '1');
         
         $typeModel = Kwf_Model_Abstract::getInstance('Linkdata');
-        $typeSelect = $typeModel->select()->whereEquals('name', 'Должности');
+        $typeSelect = $typeModel->select()->whereEquals('name', 'Позиции на борту');
         
         $this->_form->add(new Kwf_Form_Field_Select('positionId', trlKwf('Position')))
         ->setValues($typeModel)
@@ -31,14 +31,12 @@ class FlightgroupController extends Kwf_Controller_Action_Auto_Form
         
         $this->_form->add(new Kwf_Form_Field_Checkbox('leader', trlKwf('KWS')));
     }
-
-    protected function _beforeInsert(Kwf_Model_Row_Interface $row)
+    
+    protected function updateReferences(Kwf_Model_Row_Interface $row)
     {
-        $row->flightId = $this->_getParam('flightId');
-
         $m1 = Kwf_Model_Abstract::getInstance('Linkdata');
         $m2 = Kwf_Model_Abstract::getInstance('Employees');
-
+        
         $s = $m1->select()->whereEquals('id', $row->positionId);
         $prow = $m1->getRow($s);
         $row->positionName = $prow->value;
@@ -48,19 +46,16 @@ class FlightgroupController extends Kwf_Controller_Action_Auto_Form
         
         $row->employeeName = (string)$prow;
     }
+
+    protected function _beforeInsert(Kwf_Model_Row_Interface $row)
+    {
+        $row->flightId = $this->_getParam('flightId');
+
+        $this->updateReferences($row);
+    }
     
     protected function _beforeSave(Kwf_Model_Row_Interface $row)
     {        
-        $m1 = Kwf_Model_Abstract::getInstance('Linkdata');
-        $m2 = Kwf_Model_Abstract::getInstance('Employees');
-        
-        $s = $m1->select()->whereEquals('id', $row->positionId);
-        $prow = $m1->getRow($s);
-        $row->positionName = $prow->value;
-        
-        $s = $m2->select()->whereEquals('id', $row->employeeId);
-        $prow = $m2->getRow($s);
-        
-        $row->employeeName = (string)$prow;
+        $this->updateReferences($row);
     }
 }
