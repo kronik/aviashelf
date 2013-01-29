157881ada48eb2b1330cb4d7f265df15a6d73189
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2013-01-29 18:12:25 +0800

    Fixed flights columns order

--- Portal/kwf-aviashelf/controllers/FlightsController.php
+++ Portal/kwf-aviashelf/controllers/FlightsController.php
@@ -21,15 +21,17 @@ class FlightsController extends Kwf_Controller_Action_Auto_Grid
         $this->_filters = array('text' => array('type' => 'TextField'));
         
         $this->_columns->add(new Kwf_Grid_Column_Button('edit'));
-        $this->_columns->add(new Kwf_Grid_Column('number', trlKwf('Number'), 70));
         $this->_columns->add(new Kwf_Grid_Column('flightStartTime', trlKwf('Time'), 50))->setRenderer('flightTimeCorrect');
-        $this->_columns->add(new Kwf_Grid_Column('subCompanyName', trlKwf('Customer'), 100));
         $this->_columns->add(new Kwf_Grid_Column('planeName', trlKwf('Bort'), 70));
+        $this->_columns->add(new Kwf_Grid_Column('routeName', trlKwf('Route'), 100));
+        $this->_columns->add(new Kwf_Grid_Column('subCompanyName', trlKwf('Customer'), 100));
         $this->_columns->add(new Kwf_Grid_Column('firstPilotName', trlKwf('KWS'), 100));
+        $this->_columns->add(new Kwf_Grid_Column('checkPilotName', trlKwf('Instructor (check)'), 200));
         $this->_columns->add(new Kwf_Grid_Column('secondPilotName', trlKwf('Second pilot'), 100));
         $this->_columns->add(new Kwf_Grid_Column('technicName', trlKwf('Technic'), 100));
         $this->_columns->add(new Kwf_Grid_Column('resquerName', trlKwf('Resquer'), 100));
-        $this->_columns->add(new Kwf_Grid_Column('checkPilotName', trlKwf('Instructor (check)'), 200));
+        $this->_columns->add(new Kwf_Grid_Column('objectiveName', trlKwf('Objective'), 100));
+        $this->_columns->add(new Kwf_Grid_Column('number', trlKwf('Number'), 70));
         $this->_columns->add(new Kwf_Grid_Column('comments', trlKwf('Comments')))->setWidth(500);
     }
     
