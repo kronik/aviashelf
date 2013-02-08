11e54b01d6767e6d8be5f9258d708338127c47df
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2013-02-08 10:30:44 +0800

    Fixed filter lists

--- Portal/kwf-aviashelf/models/Employees.php
+++ Portal/kwf-aviashelf/models/Employees.php
@@ -13,4 +13,18 @@ class Employees extends Kwf_Model_Db
     protected $_dependentModels = array(
         'EmployeeFlightRoles' => 'EmployeeFlightRoles'
     );
+    
+    protected $_toStringField = 'name';
+    
+    protected function _init()
+    {
+        parent::_init();
+        $this->_exprs['name'] = new Kwf_Model_Select_Expr_Concat(array(
+                                                                       new Kwf_Model_Select_Expr_Field('lastname'),
+                                                                       new Kwf_Model_Select_Expr_String(' '),
+                                                                       new Kwf_Model_Select_Expr_Field('firstname'),
+                                                                       new Kwf_Model_Select_Expr_String(' '),
+                                                                       new Kwf_Model_Select_Expr_Field('middlename'),
+                                                                       ));
+    }
 }
