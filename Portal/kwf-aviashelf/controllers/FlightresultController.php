c6d3a2c2b3282a5d98fd799a56f3ce5e7b9eef10
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2013-11-21 11:36:16 +0800

    Fixed flight result update

--- Portal/kwf-aviashelf/controllers/FlightresultController.php
+++ Portal/kwf-aviashelf/controllers/FlightresultController.php
@@ -37,42 +37,37 @@ class FlightresultController extends Kwf_Controller_Action_Auto_Form
         
         $this->_form->add(new Kwf_Form_Field_Checkbox('showInTotal', trlKwf('Show in total')));
     }
-
-    protected function _beforeInsert(Kwf_Model_Row_Interface $row)
+    
+    protected function updateReferences(Kwf_Model_Row_Interface $row)
     {
         $m1 = Kwf_Model_Abstract::getInstance('Linkdata');
         $m2 = Kwf_Model_Abstract::getInstance('Wstypes');
         $m3 = Kwf_Model_Abstract::getInstance('Employees');
-
+        
         $s = $m3->select()->whereEquals('id', $this->_getParam('ownerId'));
         $prow = $m3->getRow($s);
         
-        $row->ownerId = $this->_getParam('ownerId');
-        $row->ownerName = $prow;
+        $row->ownerName = (string)$prow;
         
         $s = $m1->select()->whereEquals('id', $row->typeId);
         $prow = $m1->getRow($s);
         $row->typeName = $prow->value;
-
+        
         $s = $m2->select()->whereEquals('id', $row->planeId);
         $prow = $m2->getRow($s);
         
         $row->planeName = $prow->Name;
     }
+
+    protected function _beforeInsert(Kwf_Model_Row_Interface $row)
+    {
+        $row->ownerId = $this->_getParam('ownerId');
+
+        $this->updateReferences($row);
+    }
     
     protected function _beforeSave(Kwf_Model_Row_Interface $row)
-    {        
-        $m1 = Kwf_Model_Abstract::getInstance('Linkdata');
-        $m2 = Kwf_Model_Abstract::getInstance('Wstypes');
-
-        $s = $m1->select()->whereEquals('id', $row->typeId);
-        $prow = $m1->getRow($s);
-        
-        $row->typeName = $prow->value;
-        
-        $s = $m2->select()->whereEquals('id', $row->planeId);
-        $prow = $m2->getRow($s);
-        
-        $row->planeName = $prow->Name;
+    {
+        $this->updateReferences($row);
     }
 }
