a3bc60811c43768c9bf9833faf96520d19c38095
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2013-12-18 13:28:19 +0800

    Added empty field check

--- Portal/kwf-aviashelf/controllers/FlightController.php
+++ Portal/kwf-aviashelf/controllers/FlightController.php
@@ -34,7 +34,7 @@ class FlightController extends Kwf_Controller_Action_Auto_Form_Ex
         ->setWidth(400)
         ->setAllowBlank(false);
         
-        $tab->fields->add(new Kwf_Form_Field_TimeField('flightStartTime', trlKwf('Start Time')))->setIncrement(5);
+        $tab->fields->add(new Kwf_Form_Field_TimeField('flightStartTime', trlKwf('Start Time')))->setIncrement(5)->setAllowBlank(false);
 
         $airplanesModel = Kwf_Model_Abstract::getInstance('Airplanes');
         $airplanesSelect = $airplanesModel->select();
