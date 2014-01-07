74942e35bf23ac1b58e1b2f98a1be8e35de7a152
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2014-01-07 16:09:57 +0800

    Added missing permission

--- Portal/kwf-aviashelf/controllers/FlightsetsController.php
+++ Portal/kwf-aviashelf/controllers/FlightsetsController.php
@@ -14,7 +14,8 @@ class FlightsetsController extends Kwf_Controller_Action_Auto_Grid_Ex
         
         $users = Kwf_Registry::get('userModel');
 
-        if ($users->getAuthedUserRole() == 'admin' || $users->getAuthedUserRole() == 'plan' || $users->getAuthedUserRole() == 'power') {
+        if ($users->getAuthedUserRole() == 'admin' || $users->getAuthedUserRole() == 'plan' ||
+            $users->getAuthedUserRole() == 'power' || $users->getAuthedUserRole() == 'kws') {
             $this->_columns->add(new Kwf_Grid_Column_Button('edit'));
             
             if ($users->getAuthedUserRole() == 'power') {
