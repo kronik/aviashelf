c94d20925fe079f63291fb762c3ab91cd2ab8160
Dmitry Klimkin <dmitry.klimkin@gmail.com>
2014-01-14 14:45:54 +0800

    Added auto dictionary update

--- Portal/kwf-aviashelf/controllers/LinkController.php
+++ Portal/kwf-aviashelf/controllers/LinkController.php
@@ -10,4 +10,21 @@ class LinkController extends Kwf_Controller_Action_Auto_Form
         ->setAllowBlank(false)
         ->setWidth(300);
     }
+    
+    protected function updateReferences(Kwf_Model_Row_Interface $row)
+    {
+        $linkModel = Kwf_Model_Abstract::getInstance('Linkdata');
+        $linkSelect = $linkModel->select()->whereEquals('link_id', $row->id);
+        $links = $linkModel->getRows($linkSelect);
+        
+        foreach ($links as $link) {
+            $link->name = $row->name;
+            $link->save();
+        }
+    }
+    
+    protected function _beforeSave(Kwf_Model_Row_Interface $row)
+    {
+        $this->updateReferences($row);
+    }
 }
